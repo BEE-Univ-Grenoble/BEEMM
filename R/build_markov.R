aa66913e7dd61ddf6fe684a547561868e1c8b87f
leodenoual <145660969+leodenoual@users.noreply.github.com>
2023-11-13 18:25:23 +0100

    build_markov.R
    Conditional probabilities from a Markov model

---
+++ R/build_markov.R
@@ -0,0 +1,33 @@
+#' A function to return conditional probabilities from a Markov model of a given order.
+#'
+#' @param fasta_file A fasta file, containing a list of genetic sequences.
+#' @param order_markov Order of the Markov model.
+#'
+#' @return A data frame with the following columns:
+#'   \describe{
+#'     \item{kmer}{The kmer sequence.}
+#'     \item{knowing}{The prefix of the kmer (substring from the beginning to k-1).}
+#'     \item{emission}{The last character of the kmer (substring from k to the end).}
+#'     \item{count}{The count of occurrences of the kmer.}
+#'     \item{knowing_prob}{The probability of the prefix (knowing) occurring.}
+#'     \item{knowing_lprob}{The logarithm of the knowing_prob.}
+#'     \item{prob}{The conditional probability of the kmer given the prefix.}
+#'     \item{lprob}{The logarithm of the prob.}
+#'   }
+#'
+#' @examples
+#' To build a Markov model of order 3, on a fasta file named "mus_cpg_app.fa" containing sequences.
+#' proba_markov <- build_markov("mus_cpg_app.fa", 3)
+#'
+#' @export
+#' @importFrom BEEMM read_fasta kmer_table_seq kmer_proba
+
+build_markov <- function(fasta_file, order_markov) {
+  k = order_markov + 1
+  tab <- read_fasta(fasta_file)
+  kmer_tab <- kmer_table_seq(tab, k)
+  proba <- kmer_proba(kmer_tab)
+}
+
+
+
